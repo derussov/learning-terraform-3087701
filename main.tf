247583029c807838fabdc9a4bf7c15d1867e3b5c
derussov <47636565+derussov@users.noreply.github.com>
2024-01-17 12:00:01 -0500

    Security Group Create

--- main.tf
+++ main.tf
@@ -35,8 +35,7 @@ resource "aws_security_group" "blog" {
 
   vpc_id = data.aws_vpc.default.id
 
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
+ingress {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
@@ -45,8 +44,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
+ingress {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
@@ -55,8 +53,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
+egress {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
