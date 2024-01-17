88818d876d4d36fab7585d08acdf7533f1e12ddf
derussov <47636565+derussov@users.noreply.github.com>
2024-01-17 11:56:29 -0500

    Security Group Create

--- main.tf
+++ main.tf
@@ -34,14 +34,13 @@ resource "aws_security_group" "blog" {
   description = "Allow http and https in.  Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
-}
 
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0.0/32"]
+  cidr_blocks = ["0.0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -51,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0.0/32"]
+  cidr_blocks = ["0.0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -61,7 +60,8 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0.0/32"]
+  cidr_blocks = ["0.0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
+}
 }
