958b19451bb85c32a080753274fa195d02040ef1
derussov <47636565+derussov@users.noreply.github.com>
2024-01-17 15:43:27 -0500

    load balancer

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ module "alb" {
   version = "9.4.1"
 
   name            = "blog-alb"
-  vpc_id          = "module.blog_vpc.vpc_id"
-  subnets         = "module.blog_vpc.public_subnets"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
   # Security Group
