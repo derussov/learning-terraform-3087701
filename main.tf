f5374469ba9bc8c33c6eb00cbd296d646f235ea9
derussov <47636565+derussov@users.noreply.github.com>
2024-01-18 10:52:34 -0500

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -52,67 +52,33 @@ module "autoscaling" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "9.4.1"
+  version = "~> 6.0"
 
   name            = "${var.environment.name}-blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "${var.environment.network_prefix}.0.0/16"
-    }
-  }
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
   target_groups = {
-    ex-instance = {
+    {
       name_prefix      = "${var.environment.name}"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      }
+    }
   }
 
+http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+  
   tags = {
     Environment = var.environment.name
   }
